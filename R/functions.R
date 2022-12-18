0844abf330ef1e0ce55c6965d6c55786639311e1
Ken Butler <ken.butler@utoronto.ca>
2022-12-17 22:31:51 -0500

    Initial commit

---
+++ R/functions.R
@@ -0,0 +1,13 @@
+read_data <- function(url) {
+  read_delim(url, " ")
+}
+
+plot_lines <- function(x) {
+  ggplot(x, aes(x = speed, y = scrap, colour = line)) +
+    geom_point() +
+    geom_smooth(method = "lm")
+}
+
+fit_model1 <- function(x) {
+  lm(scrap ~ speed + line, data = x)
+}
---
+++ _targets.R
@@ -0,0 +1,35 @@
+# Created by use_targets().
+# Follow the comments below to fill in this target script.
+# Then follow the manual to check and run the pipeline:
+#   https://books.ropensci.org/targets/walkthrough.html#inspect-the-pipeline # nolint
+
+# Load packages required to define the pipeline:
+library(targets)
+library(tarchetypes) # Load other packages as needed. # nolint
+
+# Set target options:
+tar_option_set(
+  packages = c("tidyverse"), # packages that your targets need to run
+  format = "rds" # default storage format
+  # Set other options as needed.
+)
+
+# tar_make_clustermq() configuration (okay to leave alone):
+options(clustermq.scheduler = "multicore")
+
+# tar_make_future() configuration (okay to leave alone):
+# Install packages {{future}}, {{future.callr}}, and {{future.batchtools}} to allow use_targets() to configure tar_make_future() options.
+
+# Run the R scripts in the R/ folder with your custom functions:
+tar_source("R/functions.R")
+# source("other_functions.R") # Source other scripts as needed. # nolint
+
+# Replace the target list below with your own:
+list(
+  tar_download(file1, "http://ritsokiguess.site/datafiles/soap.txt",
+              paths = "data/soap.txt"),
+  tar_target(soap, read_data(file1)),
+  tar_target(plot1, plot_lines(soap)),
+  tar_target(model1, fit_model1(soap)),
+  tar_render(final_report, "report/report.Rmd")
+)
